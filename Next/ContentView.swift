be8591066be02455cb26ee0465743b85e265c0c3
SABITOV Danil <Danil.SABITOV@raiffeisen.ru>
2024-05-07 19:21:41 +0300

    removed_timer

--- Next/ContentView.swift
+++ Next/ContentView.swift
@@ -10,13 +10,9 @@ import Combine
 
 struct ContentView: View {
     @State var tapped = false
-    @State var animationAmount: Int = .zero
-    @State var timer = Timer.publish(every: 1, on: .main, in: .common)
     
     var body: some View {
         Button(action: {
-            timer = Timer.publish(every: 1, on: .main, in: .common)
-            _ = timer.connect()
             withAnimation(.interpolatingSpring(stiffness: 50, damping: 9).speed(1.5)) {
                 tapped = true
             } completion: {
@@ -37,21 +33,7 @@ struct ContentView: View {
                         .transition(.scale(.zero))
                 }
             }
-        }).onReceive(timer) { _ in
-            guard animationAmount <= 3 else {
-                self.timer.connect().cancel()
-                self.animationAmount = .zero
-                return
-            }
-            
-            withAnimation(.interpolatingSpring(stiffness: 50, damping: 9).speed(1.5)) {
-                tapped = true
-            } completion: {
-                tapped = false
-            }
-            
-            animationAmount += 1
-        }
+        })
         
         .buttonStyle(.plain)
         .font(.largeTitle)
